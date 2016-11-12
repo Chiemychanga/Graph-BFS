a5a23e962a831b50ce4f001364d32b834e7cda52
Larry Chiem <clarry@pdx.edu>
2016-11-11 20:54:08 -0800

    Youtube Link

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 /* Task: In this program, we will use a graph to represent
- * relationships by categorizing them. Here is what I made my graph
- * look like. 
+ * relationships by categorizing them. 
+ * I found a great video explaining about breadth first search graphs 
+ * and decided to build a graph based off of the example. Here
+ * is the link: 
+ * https://www.youtube.com/watch?v=QRq6p9s8NVg
  *
+ * Here is what I made my graph
+ * look like. 
  * BFS Answers:
  *
  * BASCG
